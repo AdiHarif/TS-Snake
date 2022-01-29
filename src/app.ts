46f112eb6d7b28ff2fd4b180cfb8f7dca402ecd9
Adi Harif <adi.harif@campus.technion.ac.il>
2022-01-29 22:18:47 +0200

    refactored game struct to a class

--- src/app.ts
+++ src/app.ts
@@ -4,79 +4,79 @@ import { Snake } from "./snake.js";
 import { Board , CellContent } from "./board.js";
 import { initInput, pending_direction } from "./input.js"
 
-type Game = {
-	snake: Snake;
-	board: Board;
-	last_frame_timestamp: number;
-	board_element: HTMLElement;
+class Game {
+	private snake: Snake;
+	private board: Board;
+	private last_frame_timestamp: number;
+	private board_element: HTMLElement;
+
+	constructor(board_size: number, private snake_speed: number) {
+		this.board = new Board(board_size);
+		this.snake = new Snake(this.board);
+		this.last_frame_timestamp = 0;
+		this.board_element = document.getElementById('board');
+
+		this.board.placeApple();
+	}
+	
+	update(this: Game): void {
+		this.snake.updateDirection(pending_direction);
+		this.snake.advance();
+	}
+
+	private drawCell(this:Game, pos: Position): void {
+		const element: HTMLElement = document.createElement('div');
+		element.style.gridRowStart = (pos.row + 1).toString();
+		element.style.gridColumnStart = (pos.col + 1).toString();
+		const element_type: CellContent = this.board.cells[pos.row][pos.col];
+		switch (element_type) {
+			case CellContent.SNAKE: {
+				element.classList.add('snake');
+				break;
+			}
+			case CellContent.SNAKE_HEAD:{
+				element.classList.add('snake-head');
+				break;
+			}
+			case CellContent.APPLE:{
+				element.classList.add('apple');
+				break;
+			}
+		}
+		this.board_element.appendChild(element);
+	}
+	
+	draw(this: Game): void {
+		this.board_element.innerHTML = '';
+		this.snake.locations.forEach((pos:Position) => { this.drawCell(pos);});
+		this.drawCell(this.board.getApplePosition());
+	}
+	
+	private gameLoop(this:Game, current_time: number): void {
+		window.requestAnimationFrame((time:number) => this.gameLoop(time));
+		const frame_duration: number = (current_time - this.last_frame_timestamp) / 1000;
+		if (frame_duration < 1 / this.snake_speed) {
+			return;
+		}
+		this.last_frame_timestamp = current_time;
+		this.update();
+		this.draw();
+	}
+	
+	start() {
+		this.gameLoop(0);
+	}
 }
 
 let game: Game;
 
-function update(game: Game): void {
-	game.snake.updateDirection(pending_direction);
-	game.snake.advance();
-
-}
-
 const board_size: number = 21;
 const snake_speed: number = 10;
 
-function initGame(): void {
-	let game_board: Board = new Board(board_size);
-	game = {
-		snake: new Snake(game_board),
-		board: game_board,
-		last_frame_timestamp: 0,
-		board_element: document.getElementById('board')
-	}
-
-	game.board.placeApple();
-}
-
-function drawCell(pos: Position): void {
-	const element: HTMLElement = document.createElement('div');
-	element.style.gridRowStart = (pos.row + 1).toString();
-	element.style.gridColumnStart = (pos.col + 1).toString();
-	const element_type: CellContent = game.board.cells[pos.row][pos.col];
-	switch (element_type) {
-		case CellContent.SNAKE: {
-			element.classList.add('snake');
-			break;
-		}
-		case CellContent.SNAKE_HEAD:{
-			element.classList.add('snake-head');
-			break;
-		}
-		case CellContent.APPLE:{
-			element.classList.add('apple');
-			break;
-		}
-	}
-	game.board_element.appendChild(element);
-}
-
-function draw(game: Game): void {
-	game.board_element.innerHTML = '';
-	game.snake.locations.forEach(drawCell);
-	drawCell(game.board.getApplePosition());
-}
-
-function gameLoop(current_time: number): void {
-	window.requestAnimationFrame(gameLoop);
-	const frame_duration: number = (current_time - game.last_frame_timestamp) / 1000;
-	if (frame_duration < 1 / snake_speed) {
-		return;
-	}
-	game.last_frame_timestamp = current_time;
-	update(game);
-	draw(game);
-}
-
 function main(): void {
 	initInput();
-	initGame();
-	gameLoop(game.last_frame_timestamp);
+	game = new Game(board_size, snake_speed);
+	game.start();
 }
 
 main();
