578069d3e05a45f2aa447d9fdbc5bec6f88c786a
Adi Harif <adi.harif@campus.technion.ac.il>
2022-01-30 20:00:06 +0200

    added board borders and padding around the board

--- src/graphics.ts
+++ src/graphics.ts
@@ -7,10 +7,14 @@ const SNAKE_COLOR: string = "lawngreen";
 const SNAKE_HEAD_COLOR: string = "green";
 const APPLE_COLOR: string = "red";
 const BORDER_COLOR: string = "black";
+const BACKGROUNG_COLOR: string = "white";
 
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
 let board_size: number;
+let board_start: number;
+let board_end: number;
+let cell_size: number;
 
 export function initCanvas(): void {
 	canvas = <HTMLCanvasElement>document.getElementById('canvas');
@@ -22,17 +26,22 @@ export function clearCanvas(): void {
 	resizeCanvas();
 	ctx.beginPath();
 	ctx.clearRect(0, 0, canvas.height, canvas.width);
-	ctx.fillStyle = "white";
+	ctx.fillStyle = BACKGROUNG_COLOR;
 	ctx.fillRect(0, 0, canvas.height, canvas.width);
 }
 
 function resizeCanvas(){
-	let size: number = Math.min(window.innerHeight, window.innerWidth);
-	canvas.height = size;
-	canvas.width = size;
+	const window_size: number = Math.min(window.innerHeight, window.innerWidth);
+	canvas.height = window_size;
+	canvas.width = window_size;
+	cell_size = window_size / (board_size + 2);
+	board_start = cell_size;
+	board_end = window_size - cell_size;
 }
 
-function drawLine(x1: number, y1: number, x2: number, y2: number): void {
+function drawLine(x1: number, y1: number, x2: number, y2: number, color: string, width: number): void {
+	ctx.strokeStyle = color;
+	ctx.lineWidth = width;
 	ctx.beginPath();
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
@@ -41,20 +50,17 @@ function drawLine(x1: number, y1: number, x2: number, y2: number): void {
 
 function drawGrid(): void {
 	let canvas_size: number = canvas.width;
-	let interval: number =  canvas_size / board_size;
-	ctx.strokeStyle = GRID_COLOR;
-	ctx.lineWidth = 1;
+	let interval: number =  cell_size;
 	for (let i = 1; i < board_size; i++) {
-		let line_pos = interval * i;
-		drawLine(line_pos, 0, line_pos, canvas_size);
-		drawLine(0, line_pos, canvas_size, line_pos);
+		let line_pos = board_start + (interval * i);
+		drawLine(line_pos, board_start, line_pos, board_end, GRID_COLOR, 1);
+		drawLine(board_start, line_pos, board_end, line_pos, GRID_COLOR, 1);
 	}
 }
 
 function fillCell(row: number, col: number, content: CellContent): void {
-	let rect_size: number =  canvas.width / board_size;
-	let y: number  = row * rect_size;
-	let x: number  = col * rect_size;
+	let y: number  = board_start + (row * cell_size);
+	let x: number  = board_start + (col * cell_size);
 	let color: string;
 	switch (content) {
 		case CellContent.SNAKE: {
@@ -72,17 +78,16 @@ function fillCell(row: number, col: number, content: CellContent): void {
 
 	ctx.beginPath();
 	ctx.fillStyle = color;
-	ctx.fillRect(x, y, rect_size, rect_size);
+	ctx.fillRect(x, y, cell_size, cell_size);
 	ctx.lineWidth = 3;
 	ctx.strokeStyle = BORDER_COLOR;
-	ctx.strokeRect(x, y, rect_size, rect_size);
+	ctx.strokeRect(x, y, cell_size, cell_size);
 }
 
 function drawApple(row: number, col: number): void {
-	let rect_size: number =  canvas.width / board_size;
-	let y: number  = (row + 0.5) * rect_size;
-	let x: number  = (col + 0.5) * rect_size;
-	let radius: number = rect_size / 3;
+	let y: number  = board_start + ((row + 0.5) * cell_size);
+	let x: number  = board_start + ((col + 0.5) * cell_size);
+	let radius: number = cell_size / 3;
 
 	ctx.beginPath();
 	ctx.fillStyle = APPLE_COLOR;
@@ -93,9 +98,17 @@ function drawApple(row: number, col: number): void {
 	ctx.stroke();
 }
 
+function drawBorder(): void {
+	ctx.strokeStyle = BORDER_COLOR;
+	ctx.lineWidth = 5;
+	const board_pixel_width: number = board_size * cell_size;
+	ctx.strokeRect(board_start, board_start, board_pixel_width, board_pixel_width);
+}
+
 export function drawBoard(board: Board): void {
 	board_size = board.size;
 	drawGrid();
+	drawBorder();
 	for (let i = 0; i < board_size; i++){
 		for (let j = 0; j < board_size; j++){
 			fillCell(i, j, board.content(new Position(i,j)));
